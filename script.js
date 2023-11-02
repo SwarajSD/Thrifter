8d2f98ccacc8395cc15aa75ebbfb8dd5996957ad
Swaraj11 <swaraj.derkar@gmail.com>
2023-11-02 16:19:51 +0530

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+    $(document).ready(function(){
+        $('.carousel').carousel();
+    });
+
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+    });
